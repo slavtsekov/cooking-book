94264af3f45ce2ac730b6221ba6d1d0913433138
Slav Tsekov <slav.tsekov@gmail.com>
2018-08-01 00:45:08 +0300

    Add initial recipe list implementation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from "../recipe.model";
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Tripe Soup", "Recipe for tripe soup", "http://www.questbg.com/images/stories/foodanddrink/recipes/kiril%20kapustin%201600x1200.jpg"),
+    new Recipe("Kebapcheta", "Recipe for kebapcheta", "https://i.pinimg.com/originals/71/67/71/71677198305e1e50ce12d5e30ca72dc2.jpg")
+  ];
 
   constructor() { }
 
