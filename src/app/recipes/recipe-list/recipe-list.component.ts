7a2405f0e19b2a9b82d0005ec265350a88a6d121
Slav Tsekov <slav.tsekov@gmail.com>
2018-08-08 22:18:31 +0300

    Enable recipe selection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from "../recipe.model";
 
@@ -13,9 +13,15 @@ export class RecipeListComponent implements OnInit {
     new Recipe("Kebapcheta", "Recipe for kebapcheta", "https://i.pinimg.com/originals/71/67/71/71677198305e1e50ce12d5e30ca72dc2.jpg")
   ];
 
+  @Output() select = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelect(recipe: Recipe) {
+    this.select.emit(recipe);
   }
 
 }
