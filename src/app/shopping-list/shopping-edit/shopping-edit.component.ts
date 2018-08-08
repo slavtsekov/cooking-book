f4ede827c1bb4977e2f73f5f910f10fde11e89b3
Slav Tsekov <slav.tsekov@gmail.com>
2018-08-08 22:39:39 +0300

    Enable adding of new ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+
+  @Output() addIngredient = new EventEmitter<Ingredient>()
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onIngredientAdd() {
+    this.addIngredient.emit(this.createIngredient());
+  }
+
+  createIngredient() {
+    const name = this.nameInput.nativeElement.value.trim();
+    const amount = this.amountInput.nativeElement.value;
+    if (name === '' && amount === '') return;
+    return new Ingredient(name, parseInt(amount, 10));   
   }
 
 }
