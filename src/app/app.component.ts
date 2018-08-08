84e4ee0ea1f216736108ddb9be35e651d6908d20
Slav Tsekov <slav.tsekov@gmail.com>
2018-08-08 21:51:07 +0300

    Add basic navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
 
+  onFeatureSelected(value: string) {
+    this.loadedFeature = value;
+  }
+
+  isFeatureLoaded(value: string): boolean {
+    return this.loadedFeature === value;
+  }
 }
