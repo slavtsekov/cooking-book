303e2fc6b05120930d296fc104b9c539ee672384
Slav Tsekov <slav.tsekov@gmail.com>
2018-09-01 19:36:33 +0300

    Setup routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
-
-  onFeatureSelected(value: string) {
-    this.loadedFeature = value;
-  }
-
-  isFeatureLoaded(value: string): boolean {
-    return this.loadedFeature === value;
-  }
-}
+export class AppComponent {}
