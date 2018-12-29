7c45a5172dbd1222784558715bec76181afe55d9
Slav Tsekov <slav.tsekov@gmail.com>
2018-12-29 14:59:31 +0200

    Setting up Firebase SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyByeyIynzC1Lc-_vztST6fR1IVVpFcLBgM',
+      authDomain: 'ng-cooking-book-37d0f.firebaseapp.com',
+    });
+  }
+}
