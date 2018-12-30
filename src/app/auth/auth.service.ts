fc2937c89022b507872339edef8f2c95edd78366
Slav Tsekov <slav.tsekov@gmail.com>
2018-12-30 16:25:25 +0200

    Add redirect and token saving on signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,25 +6,25 @@ import { Injectable } from '@angular/core';
 export class AuthService {
     token: string;
 
+    private startSession = () => {
+        this.router.navigate(['/']);
+        firebase.auth().currentUser.getIdToken()
+            .then(
+                (token: string) => this.token = token
+            );
+    }
+
     constructor(private router: Router) {}
 
     signupUser(email: string, password: string) {
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
-            console.error(error);
-        });
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(this.startSession)
+            .catch(error => console.error(error));
     }
 
     signinUser(email: string, password: string) {
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(
-                response => {
-                    this.router.navigate(['/']);
-                    firebase.auth().currentUser.getIdToken()
-                        .then(
-                            (token: string) => this.token = token
-                        );
-                }
-            )
+            .then(this.startSession)
             .catch(error => console.error(error));
     }
 
