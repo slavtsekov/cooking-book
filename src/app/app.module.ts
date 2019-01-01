6265eaa621ee504f7ff2929fbe5f77dc30620419
Slav Tsekov <slav.tsekov@gmail.com>
2019-01-01 22:31:18 +0200

    Add home page and lazy load recipes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RecipesModule,
     SharedModule,
     ShoppingListModule,
     AuthModule,
