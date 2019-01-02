db1d5f86b71cd9e77a263712fa74a20b97b6c359
Slav Tsekov <slav.tsekov@gmail.com>
2019-01-02 15:44:57 +0200

    Add preloading of lazy-loaded modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomeComponent } from './core/home/home.component';
 import { AuthGuard } from './auth/auth-guard.service';
@@ -22,7 +22,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
   providers: [AuthGuard]
 })
