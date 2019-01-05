b800729e2e3aec6b65e2d3cb840fe08936c1d06b
Slav Tsekov <slav.tsekov@gmail.com>
2019-01-05 16:22:45 +0200

    Add auth actions

---
+++ src/app/auth/store/auth.actions.ts
@@ -0,0 +1,26 @@
+import { Action } from '@ngrx/store';
+
+export const SIGNUP = 'SIGNUP';
+export const SIGNIN = 'SIGNIN';
+export const LOGOUT = 'LOGOUT';
+export const SET_TOKEN = 'SET_TOKEN';
+
+export class SignupAction implements Action {
+    readonly type = SIGNUP;
+}
+
+export class SigninAction implements Action {
+    readonly type = SIGNIN;
+}
+
+export class LogoutAction implements Action {
+    readonly type = LOGOUT;
+}
+
+export class SetTokenAction implements Action {
+    readonly type = SET_TOKEN;
+
+    constructor(public payload: string) {}
+}
+
+export type AuthActions = SignupAction | SigninAction | LogoutAction | SetTokenAction;
